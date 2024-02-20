ae023e8832f8eb968e6e9ea2490faa7ef7c510bc
Jacek Bastin <jacek.bastin@gmail.com>
2024-02-20 14:56:29 +0000

    Refactored form so that checkboxe shows only for new record

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,7 +12,7 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     @task.save
-    redirect_to tasks_path(@task)
+    redirect_to tasks_path
   end
 
   def update
